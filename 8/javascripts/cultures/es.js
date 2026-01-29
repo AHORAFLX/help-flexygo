d834d8854fe4ea1432210276fe3d7b2abd9c18c9
mnavarro <mnavarro@ahora.es>
2026-01-29 15:13:47 +0100

    Removed 8 with mike 2.1.3

--- 8/javascripts/cultures/es.js
+++
@@ -1,6 +0,0 @@
-FH_CULTURES.es = {
-    copied: "Copiado en el portapapeles",
-    filter_charts: "Filtrar gráficos",
-    unfilter_charts: "Quitar filtro de los gráficos",
-    flexygo_URL_modal_title: "Escribe aquí la URL de tu flexygo:",
-}
